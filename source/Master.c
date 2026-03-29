f6de6017540f85d359551af8586b76657d862178
Pixie Zenne <231278179+pixie-zenne@users.noreply.github.com>
2026-03-29 21:10:30 +0800

    Placed the foundation of the mini project

---
+++ source/Master.c
@@ -0,0 +1,34 @@
+#define master main
+
+/*
+ *
+ */
+
+#include <stdio.h>
+
+// Basic variables
+int a = 0, b = 0;
+
+// Basic Operations
+
+int addition(void) {
+  return 0;
+}
+
+int subtraction(void) {
+  return 0;
+}
+
+int multiplication(void) {
+  return 0;
+}
+
+int division(void) {
+  return 0;
+}
+
+int master(void) {
+  // Edit variables
+  // Do function
+  return 0;
+}
